dd4b2faa70c1274d55af955c0292a6a7a6af4058
shivanshu <shivshrit2@gmail.com>
2018-07-09 16:29:32 +0530

    printing pyramid

---
+++ starpyramid.rb
@@ -0,0 +1,18 @@
+class Pyramid
+  def print_star
+    space = 10
+    (0...10).each do |i|
+      (0..space).each do
+        printf " "
+      end
+     (0...2*i+1).each do
+       printf "*"
+     end
+     puts "\n"
+     space -=1
+    end
+  end
+end
+
+p = Pyramid.new
+p.print_star
